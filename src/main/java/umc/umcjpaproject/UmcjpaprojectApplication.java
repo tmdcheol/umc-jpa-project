83748f2e534f118a49c31a28c9ef145748a948da
tmdcheol <leesc08392@gmail.com>
2024-11-08 14:05:12 +0900

    feat: first commit

---
+++ src/main/java/umc/umcjpaproject/UmcjpaprojectApplication.java
@@ -0,0 +1,13 @@
+package umc.umcjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcjpaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcjpaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/umcjpaproject/UmcjpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.umcjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcjpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
